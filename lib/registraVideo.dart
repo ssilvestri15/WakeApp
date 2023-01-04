79d7d1e27ed67c66832cd04bf4731b878ba0b22f
Carmine <c.pastore35@studenti.unisa.it>
2023-01-04 17:35:33 +0100

    modificare shcermata record and send video

--- lib/registraVideo.dart
+++ lib/registraVideo.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:wakeapp/recordVideoAndSend.dart';
 
 class RegistraVideo extends StatelessWidget {
   const RegistraVideo({super.key});
@@ -7,22 +9,41 @@ class RegistraVideo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: const Color.fromRGBO(239, 232, 204, 1),
-        body: Center(
-          heightFactor: 2.5,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Image.asset('assets/images/illustrazione1.png', width: 237),
-              const Text('Come ti senti oggi?',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.arrow_circle_right_outlined, size: 82),
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              padding: EdgeInsets.only(bottom: 50),
+              child: Image.asset('assets/images/illustrazione1.png', width: 237),
+
+            ),
+            Container(
+              padding: EdgeInsets.only(bottom: 50),
+              child: const Text('Come ti senti oggi?',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)
+              ),
+
+            ),
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                  child: Text('vai avanti'),
+                    onPressed: () {
+                      Navigator.push(context, CupertinoPageRoute(builder: (context) => const CameraPage()));
+                    },
+                  ),
+                ],
               )
-            ],
-          ),
-        ));
+            ),
+
+          ],
+        ),
+      ),
+    );
 
     throw UnimplementedError();
   }
