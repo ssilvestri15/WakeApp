b195d2c56e5991f53c4b55f0feead4c72b3f422d
Carmine <c.pastore35@studenti.unisa.it>
2023-01-12 11:04:13 +0100

    add replay video page

--- lib/video_page.dart
+++ lib/video_page.dart
@@ -4,6 +4,7 @@ import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:wakeapp/invioUmore.dart';
 
 class VideoPage extends StatefulWidget {
   final String filePath;
@@ -34,29 +35,97 @@ class _VideoPageState extends State<VideoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Preview'),
-        elevation: 0,
-        backgroundColor: Colors.black26,
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.check),
-            onPressed: () {
-              print('ciao sto wia ');
-
-            },
-          )
-        ],
-      ),
-
-      extendBodyBehindAppBar: true,
       body: FutureBuilder(
         future: _initVideoPlayer(),
         builder: (context, state) {
           if (state.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
+            return const Center(
+                child: CircularProgressIndicator());
           } else {
-            return VideoPlayer(_videoPlayerController);
+            return Container(
+              color: Color.fromRGBO(255, 222, 232, 1),
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 50, bottom: 50),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          SizedBox(
+                            width: 150,
+                            child: Text('Vuoi inviare il video?',
+                              overflow: TextOverflow.clip,
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight:FontWeight.bold,
+                                decoration: TextDecoration.none,
+                                color: Color.fromRGBO(81, 48, 14, 1),
+                              ),
+                            ),
+                          ),
+                          Image(
+                            width: 150,
+                            image: AssetImage('assets/images/racconta.png'),
+                          )
+                        ],
+                      ),
+                      Container(
+                       height: 415.2,
+                        width: 311.4,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: VideoPlayer(_videoPlayerController),
+                        ),
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            style: ElevatedButton.styleFrom(
+                              shape: const StadiumBorder(),
+                              minimumSize: const Size(150, 40),
+                              backgroundColor: const Color.fromRGBO(81, 48, 14, 1),
+                            ),
+                            child: const Text('Annulla',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 23,
+                              ),
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(context, MaterialPageRoute(builder: (context)=> const InvioUmore()));
+                            },
+                            style: ElevatedButton.styleFrom(
+                              shape: const StadiumBorder(),
+                              minimumSize: const Size(150, 40),
+                              backgroundColor: const Color.fromRGBO(81, 48, 14, 1),
+                            ),
+                            child: const Text('Invia',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 23,
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            );
           }
         },
       ),
