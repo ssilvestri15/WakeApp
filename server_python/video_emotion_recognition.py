98383e86475a5e3ff277beef2712d9d88fc9a0b3
ssilvestri15 <Ss112123!>
2023-02-06 10:24:49 +0100

    ai_video

--- server_python/video_emotion_recognition.py
+++ server_python/video_emotion_recognition.py
@@ -1,36 +1,77 @@
-from fer import Video
-from fer import FER
+import cv2
+import os
+from deepface import DeepFace
+from time import monotonic
+from pathlib import Path
+
+
+def remove_text_after_last_delimiter(s, delimiter):
+    return s.rsplit(delimiter, 1)[0]
 
 def analyzeVideo(filename):
-    video = Video(filename)
 
-    # Analyze video, displaying the output
-    detector = FER(mtcnn=True)
-    raw_data = video.analyze(detector,
-                             display=False,
-                             save_frames=False,
-                             save_video=False,
-                             annotate_frames=False,
-                             zip_images=False)
-    df = video.to_pandas(raw_data)
-    df = video.get_first_face(df)
-    vid_df = video.get_emotions(df)
+    start_time = monotonic()
+    try:
+      
+        # creating a folder named data
+        if not os.path.exists('data'):
+            os.makedirs('data')
+  
+    # if not created then raise error
+    except OSError:
+        print ('Error: Creating directory of data')
 
-    angry = sum(vid_df.angry)
-    disgust = sum(vid_df.disgust)
-    fear = sum(vid_df.fear)
-    happy = sum(vid_df.happy)
-    sad = sum(vid_df.sad)
-    surprise = sum(vid_df.surprise)
-    neutral = sum(vid_df.neutral)
-    emotions = ['arrabbiato', 'disgustato', 'impaurito', 'felice', 'triste', 'sorpreso', 'neutrale']
-    emotions_values = [angry, disgust, fear, happy, sad, surprise, neutral]
-    topEmotion = max(emotions_values)
-    index = emotions_values.index(topEmotion)
-    print(index)
-    print(emotions[index])
-    print(topEmotion)
-    return topEmotion
+    # Read the video from specified path
+    cam = cv2.VideoCapture(filename)
+
+    # frame
+    currentframe = 0
+    emo = dict()
+
+    p = Path(filename)
+    p.parents[0].mkdir(parents=True, exist_ok=True)
+
+    while(True):
+      
+        # reading from frame
+        ret,frame = cam.read()
+  
+        if ret:
+            # if video is still left continue creating images0
+            name = Path(p,f"/frames/{str(currentframe)}.jpg")
+            print ('Creating...' + str(name))
+  
+            # writing the extracted images
+            cv2.imwrite(name, frame)
+  
+            # increasing counter so that it will
+            # show how many frames are created
+            currentframe += 1
+
+            em = DeepFace.analyze(img_path = name, actions = ['emotion'])
+
+            y = em[0]['dominant_emotion']
+
+            if y in emo:
+                emo[y] += 1
+            else:
+                emo[y] = 1
+
+        else:
+            break
+  
+    # Release all space and windows once done
+    cam.release()
+
+    m = max(emo.values())
+
+    for key in emo.keys():
+        emo[key] = (emo[key]/m)
+
+    print(emo)
+
+    print(f"TIME: {monotonic() - start_time}s")
+    return emo
 
 if __name__ == "__main__":
     analyzeVideo('C:\\Users\\silve\\Downloads\\test.MOV')
